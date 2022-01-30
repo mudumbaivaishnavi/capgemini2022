d69dc429e0170637a42e13fac9bd174098a28710
mudumbaivaishnavi <vaishnavimudumbai5@gmail.com>
2022-01-30 23:59:23 +0530

    my day1 java prgs

---
+++ day 1/demo/ASCIIValueOfACharacter.java
@@ -0,0 +1,15 @@
+package com.demo;
+
+public class ASCIIValueOfACharacter {
+	public static void main(String[] args) {
+
+        char ch = 'b';
+        int ascii = ch;
+        // You can also cast char to int
+        int castAscii = (int) ch;
+
+        System.out.println("The ASCII value of " + ch + " is: " + ascii);
+        System.out.println("The ASCII value of " + ch + " is: " + castAscii);
+    }
+
+}
