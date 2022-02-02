c06c205f995aa92a5f362947261aeb3660f0402b
mudumbaivaishnavi <vaishnavimudumbai5@gmail.com>
2022-02-02 12:34:51 +0530

    my day 4 java prgs

---
+++ day4/PositiveOrNegativeOrZero.java
@@ -0,0 +1,25 @@
+package day4;
+
+public class PositiveOrNegativeOrZero 
+{  
+public static void main(String[] args)   
+{  
+//number to be check  
+int num=-1;  
+//checks the number is greater than 0 or not  
+if(num>0)  
+{  
+System.out.println("The number is positive.");  
+}  
+//checks the number is less than 0 or not  
+else if(num<0)  
+{  
+System.out.println("The number is negative.");  
+}  
+//executes when the above two conditions return false  
+else  
+{  
+System.out.println("The number is zero.");  
+}  
+}  
+}  
