b6bae2c47331842671da15cd293813dabf4f4ada
mudumbaivaishnavi <vaishnavimudumbai5@gmail.com>
2022-02-02 14:38:06 +0530

    my day 4 java prgs

---
+++ day4/CommandLineArgumentsOrNot.java
@@ -0,0 +1,17 @@
+package day4;
+
+public class CommandLineArgumentsOrNot{
+	public static void main(String[] args)
+	{
+		int k= args.length;
+		if(k==0) {
+			System.out.println("No values");
+		}
+		else {
+			System.out.print(args[0]);
+			for(int i=1;i<k;i++) {
+				System.out.print(","+ args[i]);
+			}
+		}
+	}
+}
