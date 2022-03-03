2b6ae6bb66e5d697a1f228cc1b8cf7ec8ec409d2
mudumbaivaishnavi <vaishnavimudumbai5@gmail.com>
2022-03-03 17:25:38 +0530

    bean scopes

---
+++ fuctions and springdemo/springBeanScope_singleton/src/main/MainClass.java
@@ -0,0 +1,29 @@
+package main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import pojo.Message;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		//it is a IOC-inverse of control
+		ApplicationContext applicationContext =new ClassPathXmlApplicationContext("beans.xml");//created spring container to instantiate object
+		
+		Message message1 = (Message) applicationContext.getBean("hello");
+		
+		
+		
+	//	Message m = new Message() this object was created by spring container-ApplicationContext
+		
+		System.out.println("this is my object: " + message1);
+		
+		Message message2 = (Message) applicationContext.getBean("hello");
+		
+		System.out.println("this is my object m2: " + message2);
+		
+		   
+	}
+
+}
---
+++ fuctions and springdemo/springBeanScope_singleton/src/pojo/Message.java
@@ -0,0 +1,23 @@
+package pojo;
+
+
+
+public class Message {
+	//property
+	String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	public void show() {
+		
+		System.out.println(" calling without contructor object creation");
+		
+	}
+
+}
