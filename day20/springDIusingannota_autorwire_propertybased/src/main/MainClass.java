8047d6eddba4e3bc32d19ed3854dcf9330bd89ac
mudumbaivaishnavi <vaishnavimudumbai5@gmail.com>
2022-03-04 20:45:59 +0530

    spring Annotations

---
+++ day20/springDIusingannota_autorwire_propertybased/src/main/MainClass.java
@@ -0,0 +1,22 @@
+package main;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.demo.config.AppConfig;
+import com.demo.pojo.Employee;
+
+
+////using properties based
+
+public class MainClass {
+
+	public static void main(String[] args) {
+                    //spring ioc-----search for beans and here employe is bean connected with address
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
+		Employee employee = context.getBean(Employee.class);
+
+		employee.EmployeeAddress();
+
+	}
+}
